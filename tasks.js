3b6f5b585781d99f70c147e6018c344bbba4e4ed
nasty <nastylitvin1991liverpool@gmail.com>
2022-11-03 23:19:27 +0300

    task highAndLow

--- tasks.js
+++ tasks.js
@@ -42,3 +42,13 @@ function checkExam(array1, array2) {
 }
 checkExam(ar1, ar2)
 console.log(checkExam(a, b))
+
+function highAndLow(numbers){
+    function highAndLow(numbers){
+        let array = numbers.split(" ");
+        let arraySort = array.sort((a, b) => a - b)   
+        let res = `${arraySort[arraySort.length-1]}`+ ' ' + `${arraySort[0]}`
+        return res
+    }
+}
+highAndLow('1 2 8 -2 9 25')
